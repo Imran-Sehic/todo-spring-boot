444cb4a701e6dbcbbaf87ac239a33f9ecc0717c7
Imran <imran.sehic55@gmail.com>
2020-11-16 18:38:45 +0100

    added application's screenshots to the source folder

--- src/main/java/com/todoSpringBoot/controller/UserController.java
+++ src/main/java/com/todoSpringBoot/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.todoSpringBoot.controller;
 
-import com.todoSpringBoot.dto.RefreshTokenRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.todoSpringBoot.service.AuthService;
 import com.todoSpringBoot.service.UserService;
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
